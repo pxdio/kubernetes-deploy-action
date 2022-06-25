e9399256d68ace7d21bfeacb0cf9fc54480d55ea
Chris Gavin <chris@chrisgavin.me>
2022-06-25 13:22:30 +0100

    Make some inputs optional if only linting.

--- src/inputs.ts
+++ src/inputs.ts
@@ -1,10 +1,10 @@
 import * as core from "@actions/core";
 
 export class Inputs {
-	configuration = core.getInput("configuration", {required: true});
-	resources = core.getInput("resources", {required: true});
-	namespace = core.getInput("namespace", {required: true});
 	event = core.getInput("event", {required: true});
+	resources = core.getInput("resources", {required: true});
+	namespace = core.getInput("namespace", {required: this.event === "deployment"});
+	configuration = core.getInput("configuration", {required: this.event === "deployment"});
 }
 
 export default function get():Inputs {
