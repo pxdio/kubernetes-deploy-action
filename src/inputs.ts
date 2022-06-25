c1c6e15d8bbb4758a959437753946551bdf310a2
Chris Gavin <chris@chrisgavin.me>
2022-06-25 14:00:42 +0100

    Add a utility method for checking if we're deploying.

--- src/inputs.ts
+++ src/inputs.ts
@@ -3,8 +3,12 @@ import * as core from "@actions/core";
 export class Inputs {
 	event = core.getInput("event", {required: true});
 	resources = core.getInput("resources", {required: true});
-	namespace = core.getInput("namespace", {required: this.event === "deployment"});
-	configuration = core.getInput("configuration", {required: this.event === "deployment"});
+	namespace = core.getInput("namespace", {required: this.isDeployment()});
+	configuration = core.getInput("configuration", {required: this.isDeployment()});
+
+	isDeployment(): boolean {
+		return this.event === "deployment";
+	}
 }
 
 export default function get():Inputs {
